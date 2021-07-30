127df96997268f4b35654f38879df8c18e55f7ad
trunghieupython10102001 <78651715+trunghieupython10102001@users.noreply.github.com>
2021-07-30 18:05:39 +0700

    rewrite code

--- app.js
+++ app.js
@@ -1,171 +1,273 @@
-let play = document.querySelector(".play")
-let audio = document.querySelector('.audio')
-let dashboard = document.querySelector('.dashboard')
-let isPlay = false
-let dashboardTimer = document.querySelector('.dashboard__timer')
-let time = document.querySelector('.time')
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+const playlist = $('.playlist')
+const dashboard = $('.dashboard')
+const dashboardImg = $('.dashboard__img')
+const songName = $('.song-name')
+const audio = $('.audio')
+const play = $('.play')
+let time = $('.time')
+const timeBar = $('.dashboard__timer')
 let timeLength = 0
-let dashboardImg = document.querySelector('.dashboard__img')
-let imgRotation = 0
+const nextBtn = $('.next')
+const backBtn = $('.back')
+const shuffleBtn = $('.shuffle')
+const replayBtn = $('.replay')
+let songList
 let rotateImg
-let playlist = document.querySelector('.playlist')
-let listSong = document.getElementsByClassName('playlist__item')
-let nextSong = document.querySelector('.next')
-let currentSong
-let currentElement
-let currentSongidx = 0
-console.log(nextSong)
+// console.log(songList)
 
-let srcList = []
+const app = {
+	currentIndex: 0,
+	shuffleList: [],
+	replay: false,
+	songs: [
+		{
+			img: "Attention",
+			name: "Attention",
+			artist: "Charlie Puth",
+			src: "Attention"
+		},
+		{
+			img: "Blank space",
+			name: "Blank space",
+			artist: "Taylor Swift",
+			src: "Blank space"
+		},
+		{
+			img: "Dusk till down",
+			name: "Dusk till down",
+			artist: "Zayn ft. Sia",
+			src: "Dusk till down"
+		},
+		{
+			img: "Girls like you",
+			name: "Girls like you",
+			artist: "Maroon 5",
+			src: "Girls like you"
+		},
+		{
+			img: "Hello",
+			name: "Hello",
+			artist: "Adele",
+			src: "Hello"
+		},
+		{
+			img: "Despacito",
+			name: "Despactio",
+			artist: "Luis Fonsi ft. Daddy Yankee",
+			src: "Despacito"
+		},
+		{
+			img: "Shape of you",
+			name: "Shape of you",
+			artist: "Ed Sheeran",
+			src: "Shape of you"
+		},
+		{
+			img: "Shape of you",
+			name: "Shape of you",
+			artist: "Ed Sheeran",
+			src: "Shape of you"
+		},
+	],
+	render() {
+		let htmls = this.songs.map(song => {
+			return `
+				<div class="playlist__item">
+					<div class="item-img">
+						<img src="./assets/img/${song.img}.jpeg" alt="song-img" class="img-song">
+					</div>
+					<div class="item-content">
+						<p class="item-content__name">${song.name}</p>
+						<p class="item-content__artist">${song.artist}</p>
+					</div>
+					<div class="item-more-info">
+						<i class="fas fa-ellipsis-h"></i>
+					</div>
+				</div>
+			`
+		})
+		playlist.style.marginTop = dashboard.offsetHeight + 'px'
+		playlist.innerHTML = htmls.join('\n')
+	},
+	defineProperties() {
+		Object.defineProperty(this, 'currentSong', {
+			get() {
+				return this.songs[this.currentIndex]
+			}
+		}) 
+	},
+	loadCurrentSong() {
+		songName.textContent = this.currentSong.name
+		dashboardImg.innerHTML = `<img src="./assets/img/${this.currentSong.img}.jpeg" alt="song-img" class="img-song">`
+		audio.src = `./assets/audio/${this.currentSong.src}.mp3`
+	},
+	handleEvents() {
+		const dashboardImgHeight = dashboardImg.offsetHeight
+		// Handle scroll event
+		document.onscroll = function() {
+			const scrollTop = window.scrollY 
+			const newHeight = dashboardImgHeight - scrollTop 
+			dashboardImg.style.height = dashboardImg.style.width = newHeight > 0 ? newHeight + 'px' : 0
+			dashboardImg.style.opacity = newHeight / 200
+		}
 
-playlist.style.marginTop = dashboard.clientHeight + 'px'
+		// Handle click event in play button
+		play.onclick = () => {
+			if (audio.paused) {
+				app.playMusic()
+			}
+			else {
+				app.pauseMusic()
+			}
+		}
 
+		// Handle timeBar click event
+		timeBar.onclick = (e) => {
+			audio.currentTime = (e.x - timeBar.offsetLeft) / timeBar.clientWidth * audio.duration
+		}
 
-
-let playlistSong = [
-	{
-		img: "Attention",
-		name: "Attention",
-		artist: "Charlie Puth",
-		src: "Attention"
-	},
-	{
-		img: "Blank space",
-		name: "Blank space",
-		artist: "Taylor Swift",
-		src: "Blank space"
-	},
-	{
-		img: "Dusk till down",
-		name: "Dusk till down",
-		artist: "Zayn ft. Sia",
-		src: "Dusk till down"
-	},
-	{
-		img: "Girls like you",
-		name: "Girls like you",
-		artist: "Maroon 5",
-		src: "Girls like you"
-	},
-	{
-		img: "Hello",
-		name: "Hello",
-		artist: "Adele",
-		src: "Hello"
-	},
-	{
-		img: "Despacito",
-		name: "Despactio",
-		artist: "Luis Fonsi ft. Daddy Yankee",
-		src: "Despacito"
-	},
-	{
-		img: "Shape of you",
-		name: "Shape of you",
-		artist: "Ed Sheeran",
-		src: "Shape of you"
-	},
+		// Rotate dashboard image
+		rotateImg = dashboardImg.animate(
+			{
+				transform: 'rotate(360deg)'
+			}, 
+			{
+				duration: 20000,
+				iterations: Infinity
+			}
+		)
+		rotateImg.pause()
 		
-]
 
-currentSong = playlistSong[0]
+		// Handle next click event
+		nextBtn.onclick = () => {
+			this.nextSong()
+			app.playMusic()
+		}
+		// Handle back click event
+		backBtn.onclick = () => {
+			this.backSong()
+			app.playMusic()
+		}
 
-time.style.width = timeLength + 'px'
+		// Handle shuffle click event
+		shuffleBtn.onclick = () => {
+			if (shuffleBtn.classList.length === 1) {
+				this.shuffleSong()
+				this.render()
+				app.playMusic()
+			} else {
+				this.songs = this.shuffleList
+				this.currentIndex = 0
+				this.loadCurrentSong()
+				this.render()
+				app.playMusic()
+			}
+			shuffleBtn.classList.toggle('clicked')
+			this.clickSong()
+		}
 
-playlistSong.forEach(item => {
-	srcList.push(item.src)
-})
+		// Handle replay click event
+		replayBtn.onclick = () => {
+			if (replayBtn.classList.length === 1) {
+				app.replay = true
+			} else {
+				app.replay = false
+			}
+			replayBtn.classList.toggle('clicked')
+		}
 
-console.log(srcList)
-
-play.addEventListener('click', (e) => {
-	if (!audio.paused) {
-		clearInterval(rotateImg)
+		// Handle songs click event
+		this.clickSong()
+	},
+	runTime() {
+		const timeRunning = setInterval(() => {
+			timeLength = audio.currentTime / audio.duration * 100 + '%'
+			time.style.width = timeLength
+			if (audio.ended) {
+				rotateImg.pause()
+			}
+			if (!this.replay) {
+				app.autoNext()
+			} else {
+				if (audio.ended) {
+					app.playMusic()
+				}
+			}
+		}, 10)
+	},
+	nextSong() {
+		this.currentIndex++
+		if (this.currentIndex >= this.songs.length) {
+			this.currentIndex = 0
+		}
+		this.loadCurrentSong()
+	},
+	playMusic() {
+		audio.play()
+		play.innerHTML = `<i class="fas fa-pause-circle"></i>`
+		rotateImg.play()
+	},
+	pauseMusic() {
 		audio.pause()
-		isPlay = false
 		play.innerHTML = `<i class="fas fa-play-circle"></i>`
-	} else {
-		audio.play()
-		play.innerHTML = `<i class="fas fa-pause-circle"></i>`
-		const timeRunning = setInterval(() => {
-			timeLength = audio.currentTime / audio.duration
-			time.style.width = timeLength * 100 + '%'
-		}, 10)
-		if (!audio.ended) {
-			rotateImg = setInterval(() => {
-				imgRotation += 0.1
-				dashboardImg.style.transform = `rotate(${imgRotation}deg)` 
-				if (audio.ended) {
-					clearInterval(rotateImg)
+		rotateImg.pause()
+	},
+	backSong() {
+		if (this.currentIndex > 0) {
+			this.currentIndex--
+		}
+		this.loadCurrentSong()
+	}, 
+	shuffleSong() {
+		this.shuffleList = JSON.parse(JSON.stringify(this.songs))
+		let maxLength = this.songs.length
+		for (let i = 0; i < this.songs.length; i++) {
+			let randomIdx = Math.floor(Math.random() * maxLength)
+			let tempSong = this.songs[i]
+			this.songs[i] = this.songs[randomIdx]
+			this.songs[randomIdx] = tempSong
+		}
+		this.currentIndex = 0
+		this.loadCurrentSong()
+	},
+	autoNext() {
+		if (audio.ended) {
+			this.currentIndex++
+			if (this.currentIndex >= this.songs.length) {
+				this.currentIndex = 0
+			}
+			this.loadCurrentSong()
+			this.playMusic()
+		}
+	},
+	replaySong() {
+		this.replay = true
+	},
+	clickSong() {
+		songList = playlist.children
+		// console.log(songList)
+		for (let i = 0; i < songList.length; i++) {
+			songList[i].onclick = function(e) {
+				app.currentIndex = i
+				app.loadCurrentSong()
+				app.playMusic()
+				for(let i = 0; i < songList.length; i++) {
+					songList[i].classList.remove('playing')
 				}
-			}, 10)
-		}		
-	}
-})
-
-dashboardTimer.onclick = (e) => {
-	audio.currentTime = (e.x - dashboardTimer.offsetLeft) / dashboardTimer.clientWidth * audio.duration
-}
-
-function addPlaylist(playlistSong) {
-	playlistSong.forEach(playlistItem => {
-		let listItem = document.createElement('div')
-		listItem.classList.add('playlist__item')
-		listItem.setAttribute('value', `${playlistItem.src}`)
-		listItem.innerHTML = `
-			<div class="item-img">
-				<img src="./assets/img/${playlistItem.img}.jpeg" alt="song-img" class="img-song">
-			</div>
-			<div class="item-content">
-				<p class="item-content__name">${playlistItem.name}</p>
-				<p class="item-content__artist">${playlistItem.artist}</p>
-			</div>
-			<div class="item-more-info">
-				<i class="fas fa-ellipsis-h"></i>
-			</div>
-		`
-		playlist.appendChild(listItem)
-	})
-}
-
-addPlaylist(playlistSong)
-
-for (let i = 0; i < listSong.length; i++) {
-	listSong[i].onclick = (e) => {
-		currentElement = listSong[i]
-		currentSong = playlistSong[i]
-		audio.src = `./assets/audio/${currentSong.src}.mp3`
-		timeLength = 0
-		audio.play()
-		dashboardImg.innerHTML = `<img src="./assets/img/${listSong[i].getAttribute('value')}.jpeg" alt="song-img" class="img-song">`
-		document.querySelector('.song-name').innerHTML = listSong[i].getAttribute('value')
-		// play
-		play.innerHTML = `<i class="fas fa-pause-circle"></i>`
-		const timeRunning = setInterval(() => {
-			timeLength = audio.currentTime / audio.duration
-			time.style.width = timeLength * 100 + '%'
-		}, 10)
-		clearInterval(rotateImg)
-		imgRotation = 0
-		if (!audio.ended) {
-			rotateImg = setInterval(() => {
-				imgRotation += 0.1
-				dashboardImg.style.transform = `rotate(${imgRotation}deg)` 
-				if (audio.ended) {
-					clearInterval(rotateImg)
-				}
-			}, 10)
-		}		
+				songList[i].classList.add('playing')
+			}
+		}	
+	},
+	start() {
+		this.defineProperties()
+		this.render()
+		this.loadCurrentSong()
+		this.handleEvents()
+		this.runTime()
 	}
 }
 
-
-
-document.onscroll = (e) => {
-	// dashboardImg.style.transform = `scale(${playlist.scrollTop / dashboardImg.clientHeight}%, ${playlist.scrollTop / dashboardImg.clientHeight}%)`
-}
-
-nextSong.onclick = e => {
-	console.log(currentSong)
-	// console.log(currentElement.)
-}
+app.start()
